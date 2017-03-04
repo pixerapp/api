75c53ee33dc79d0a9c69a8b0cac8423b8aa9ac99
Esen Sagynov <kadishmal@gmail.com>
2017-03-05 08:36:00 +0900

    Implement the middleware to handle file uploads.

---
+++ lib/files/upload.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#curl -X POST http://localhost:3000/files -F "file=@/Users/naver/repos/photos-indexer/images/IMG_4111_4.JPG"
+#curl -X POST http://localhost:3000/files -F "file=@/Users/naver/repos/photos-indexer/images/IMG_4147_4.JPG"
+#curl -X POST http://localhost:3000/files -F "file=@/Users/naver/repos/photos-indexer/images/IMG_4375_2.JPG"
+curl -X POST http://localhost:3000/files -F "file=@/Users/naver/repos/photos-indexer/images/IMG_4070.JPG"
